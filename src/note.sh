b3061c74d3cf7cff3a4202e038cc113f65790177
Sam McAlilly <sammcalilly@gmail.com>
2020-12-04 19:55:14 -0600

    added a file_name and directory argument. refactored the search to open in less.

--- src/note.sh
+++ src/note.sh
@@ -11,20 +11,47 @@ function usage() {
   echo "Usage: note.sh [action]"
   echo
   echo "  actions:"
-  echo "    grep [pattern]"
+  echo "    -f [file_name]"
+  echo "    -d [directory]"
+  echo "    -g grep [pattern]"
 }
+
+# parse any arguments
+while getopts g:d:f: flag 
+do
+    case "${flag}" in
+        f) file_name=${OPTARG};;
+        d) directory=${OPTARG};;
+        g) grp=${OPTARG};;
+    esac
+done
 
 mkdir -p "$NOTE_DIR"
 
+# if there is no argument, then write to the daily file
 if [ $# -eq 0 ]; then
-    $EDITOR "$NOTE_DIR/$(date +'%Y-%m-%d').md"
+    mkdir -p "$NOTE_DIR/daily"
+    NOTE_PATH="$NOTE_DIR/daily/$(date +'%Y-%m-%d').md"
+    $EDITOR $NOTE_PATH
+
+# decide which filename/directory & open the text editor
+elif [ -n "${directory+1}" ] || [ -n "${file_name+1}" ]; then
+    if [ -n "${directory+1}" ] && [  -n "${file_name+1}" ]; then
+        mkdir -p $NOTE_DIR/$directory
+        NOTE_PATH="$NOTE_DIR/$directory/$file_name.md"
+    elif [ -n "${directory+1}" ] && [ ! -n "${file_name+1}" ]; then
+        mkdir -p $NOTE_DIR/$directory
+        NOTE_PATH="$NOTE_DIR/$directory/$(date +'%Y-%m-%d').md"
+    else
+        NOTE_PATH="$NOTE_DIR/${file_name}.md"
+    fi
+  $EDITOR $NOTE_PATH
+
+# search with grep and open the files with less
+elif [ -n "${grp+1}" ]; then
+    notes=$(grep -nr "$2" $NOTE_DIR -l)
+    echo $notes
+    less -p "$2" $notes 
 else
-  case $1 in
-    grep)
-      grep -i -r --color "$2" "$NOTE_DIR"
-      ;;
-    *)
-      usage
-      ;;
-  esac
+    usage
 fi
