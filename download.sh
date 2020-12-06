692f7d68cc5a39939e14ab15080643c949621577
Sam McAlilly <sammcalilly@gmail.com>
2020-12-05 22:17:45 -0600

    configuring

--- download.sh
+++ download.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
-set -x
+
+set_user_editor() {
+    echo "What is the executable name for your preferred text editor (i.e. vim or nano or atom or subl)?"
+
+    echo "Type the name and hit enter: [nano]"
+    read EDITOR
+    
+    # todo bug - need to validate editor
+    echo "Thanks. We're going to use $EDITOR whenever we open any of your writing."
+}
+
+set_user_editor
 
 if [ -n "${WRITER_DIR+1}" ]; then
     echo "$WRITER_DIR"
@@ -10,11 +21,15 @@ if [ -n "${WRITER_DIR+1}" ]; then
     touch "env-variables.sh"
     touch "install.sh"
     touch "writer.sh"
-    echo "export WRITER_DIR=$HOME/$WRITER_DIR" > "env-variables.sh"
+   
+    # echo "export WRITER_DIR=$HOME/$WRITER_DIR" > "env-variables.sh"
 
-
-    curl "https://raw.githubusercontent.com/smcalilly/writer/main/writer.sh/src/writer.sh" -o write.sh
-    chmod +x write.sh
+    
+    curl "https://raw.githubusercontent.com/smcalilly/writer/main/writer.sh/src/writer.sh" | head -1 >> writer.sh
+    echo "export WRITER_DIR=$HOME/$WRITER_DIR" >> "writer.sh"
+    echo "export EDITOR=$EDITOR" >> "writer.sh"
+    curl "https://raw.githubusercontent.com/smcalilly/writer/main/writer.sh/src/writer.sh" | tail -n +2 >> writer.sh
+    chmod +x writer.sh
 
     curl "https://raw.githubusercontent.com/smcalilly/writer/main/install.sh" -o install.sh
     chmod +x install.sh
