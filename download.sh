878548b5b06b046806bbd10a2ce65e267deecd80
Sam McAlilly <sammcalilly@gmail.com>
2020-12-08 21:53:15 -0600

    revert  variable export

--- download.sh
+++ download.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-
-if [ -n "${EDITOR+1}" ]; then
+if [[ -n "${EDITOR+1}" ] && [ -n "${WRITER_DIR+1}" ]]; then
+    cd $WRITER_DIR
     mkdir -p ".src" && cd .src
     touch "install.sh"
     chmod +x install.sh
@@ -15,8 +15,9 @@ if [ -n "${EDITOR+1}" ]; then
 else
     echo "Unable to download."
     echo
-    echo "You need to change the $EDITOR variable in your shell's startup file."
+    echo "You need to set the $WRITER_DIR and $EDITOR variables in your shell's startup file."
     echo "It might look like this:"
     echo
+    echo "      export WRITER_DIR=writing"
     echo "      export EDITOR=vim"
 fi
